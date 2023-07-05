10c45b8397af6333cdd6a6e01a714733aa4eb7cf
MakeSureDudeDies <84237951+MakeSureDudeDies@users.noreply.github.com>
2023-07-05 02:25:26 +0200

    Update randomstring.js

--- js/randomstring.js
+++ js/randomstring.js
@@ -1,1 +1,8 @@
-
+var textArray = [    
+'I like green',    
+'That one guy that does stuff',
+'lua > python'
+];
+var randomIndex = Math.floor(Math.random() * textArray.length); 
+var randomElement = textArray[randomIndex];
+document.getElementById("randomstring").innerHTML = randomElement;
