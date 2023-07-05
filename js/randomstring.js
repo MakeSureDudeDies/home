440d61171007c99c90e5bde9cac67d83c0dfde24
MakeSureDudeDies <84237951+MakeSureDudeDies@users.noreply.github.com>
2023-07-05 02:36:18 +0200

    Update randomstring.js

--- js/randomstring.js
+++ js/randomstring.js
@@ -1,7 +1,8 @@
 var textArray = [    
-'I like green',    
+'I like green',
 'That one guy that does stuff',
-'lua > python'
+'lua > python',
+'just wait for some vegetable to say raknet'
 ];
 var randomIndex = Math.floor(Math.random() * textArray.length); 
 var randomElement = textArray[randomIndex];
