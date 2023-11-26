86bbc56e2825eaba38e36e3128faacf3eb58cfcc
MakeSureDudeDies <84237951+MakeSureDudeDies@users.noreply.github.com>
2023-11-27 00:09:24 +0100

    Update randomstring.js

--- js/randomstring.js
+++ js/randomstring.js
@@ -1,10 +1,23 @@
 var textArray = [    
-'I like green',
-'That one guy that does stuff',
-'lua > python',
-'just wait for some vegetable to say raknet',
-'omg look funny minecraft font'
-];
-var randomIndex = Math.floor(Math.random() * textArray.length); 
-var randomElement = textArray[randomIndex];
-document.getElementById("randomstring").innerHTML = randomElement;
+    'I like green',
+    'That one guy that does stuff',
+    'lua > python',
+    'just wait for some vegetable to say raknet',
+    'omg look funny minecraft font'
+  ];
+
+  var randomIndex = Math.floor(Math.random() * textArray.length);
+  var randomElement = textArray[randomIndex];
+  var element = document.getElementById("randomstring");
+
+  function animateText(text, index, interval) {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+      setTimeout(function () {
+        animateText(text, index, interval);
+      }, interval);
+    }
+  }
+
+  animateText(randomElement, 0, 100); // Adjust the interval as needed
